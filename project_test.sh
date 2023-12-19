01f20ac186218616de652d84bb2c731acbbcdf03
hellijah <thierry.corlieto@gmail.com>
2023-12-19 16:28:09 +0100

    colorisation

--- project_test.sh
+++ project_test.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Ajout des variables de couleur
-red='\033[0;31m'
-yellow='\033[1;31m'
-green='\033[1;32m'
+red='\e[31m'
+yellow='\e[33m'
+green='\e[32m'
+green_background='\e[42m'
 # Couleur standard
-clear='\033[0m'
+clear='\e[0m'
 
 
 # Function to configure the host name
 configure_hostname() {
     read -p "spécifier un nouveau nom d'hôte pour la machine. : " new_hostname
     sudo hostnamectl set-hostname "$new_hostname"
-    echo "Mon nouveau nom est: $new_hostname."
+    echo -e "$green Mon nouveau nom est: $new_hostname. $clear "
     
 }
 
@@ -21,47 +22,47 @@ create_new_user() {
     read -p "Entrer le nom d'utilisateur : " username
     read -p "Entrer le mot de passe pour: $username: " password
     if grep -q "$username:" /etc/passwd; then
-	    echo "l'utilisateur existe déjà"
+	    echo -e "$yellow l'utilisateur existe déjà $clear "
 	    
 	    
     else
     	sudo useradd -m -s /bin/bash "$username" &>/dev/null 
     	echo "$username:$password" | sudo chpasswd
-    	echo "User $username créé."
+    	echo -e "$green User $username créé. $clear"
     	read -p "voulez vous ajouter $username dans un groupe ? (yes/no): " add_user_to_group
 
     	if [ "$add_user_to_group" = "yes" ]; then
     		read -p "Entrer le groupe : " group_name
 
     	if grep -q "$group_name:" /etc/group; then
-        	read -p "Le groupe '$group_name' existe. Voulez-vous ajouter l’utilisateur à ce groupe? (yes/no): " add_to_existing_group
+        	read -p "$yellow Le groupe '$group_name' existe. Voulez-vous ajouter l’utilisateur à ce groupe? (yes/no): $clear " add_to_existing_group
 
         	if [ "$add_to_existing_group" = "yes" ]; then
-            		echo "¢a marche, on va ajouter $username, dans $group_name "
+            		echo -e "¢a marche, on va ajouter $username, dans $group_name "
             		sudo adduser "$username" "$group_name"
-            		echo "User '$username' est maintenant dans le groupe'$group_name'."
+            		echo -e "$green User '$username' est maintenant dans le groupe'$group_name'. $clear "
         	else
-            		echo "On arrête."
+            		echo -e "$red On arrête. $clear "
             		exit 1
 		fi
     	else
-        	read -p " le groupe '$group_name' n’existe pas. Voulez-vous créer ce groupe? (yes/no): " create_group
+        	read -p "$yellow le groupe '$group_name' n’existe pas. Voulez-vous créer ce groupe? (yes/no): $clear" create_group
 
         	if [ "$create_group" = "yes" ]; then
             		sudo groupadd "$group_name"
             		echo "Groupe '$group_name'créé ."
           
             		sudo adduser "$username" "$group_name"
-            		echo "User '$username' a été ajouté '$group_name'."
+            		echo -e "$green User '$username' a été ajouté '$group_name'. $clear "
         	else
-            		echo "le groupe n'a pas été créé."
+            		echo -e "$red le groupe n'a pas été créé. $clear"
             		exit 1
         	fi
     	fi
  	elif [ "$add_user_to_group" = "no" ]; then
-    		echo "Pas de probleme. "
+    		echo -e "$green Pas de probleme. $clear "
 	else
-    		echo "Choix non valide. Veuillez entrer 'yes' or 'no'."
+    		echo -e "$yellow Choix non valide. Veuillez entrer 'yes' or 'no'. $clear"
     		exit 1
 	fi
 
@@ -84,12 +85,12 @@ install_software() {
         sudo apt update  &>/dev/null  # c'est toujours bien de verifier que le repertory est bien à jour, donc on update toujours avant d'installer n'importe quoi.
         sudo apt install -y $software_list  # Installation de l'apt du logiciel renseigné 
         if [ $? -eq 0 ]; then #si la sorti $?=0 alors l'installation a bien été faite 
-                echo "le logiciel: $software_list, a bien été installé !"
+                echo -e "$green le logiciel: $software_list, a bien été installé ! $clear"
         else
-                echo "Il semble y avoir un problème avec l'installation . "# si $? !=0 alors il y a eu un probleme.    
+                echo -e "$red Il semble y avoir un problème avec l'installation . $clear " # si $? !=0 alors il y a eu un probleme.    
         fi
    else
-        echo "Le logiciel $software_list n'existe pas ." 
+        echo -e "$red Le logiciel $software_list n'existe pas . $clear" 
    fi
 
  } 
@@ -99,13 +100,13 @@ network_configuration() {
    
     # Vérification de la présence de dialog et installation le cas échéant
 if ! command -v dialog &> /dev/null; then
-    read -p "Le programme 'dialog' n'est pas installé. Souhaitez-vous l'installer ? (oui/non) : " install_dialog
+    read -p "$yellow Le programme 'dialog' n'est pas installé. Souhaitez-vous l'installer ? (oui/non) : $clear " install_dialog
     if [ "$install_dialog" = "oui" ]; then
         # Installation de dialog
         apt-get update
         apt-get install -y dialog
     else
-        echo "Installation de 'dialog' annulée."
+        echo -e "$red Installation de 'dialog' annulée. $clear"
         exit 1
     fi
 fi
@@ -118,24 +119,24 @@ while true; do
     read -p "Entrez le serveur DNS secondaire : " dns_secondaire
     # Vérification de la validité de l'adresse IP et du masque de sous-réseau
     if ! [[ $adresse_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo "Adresse IP invalide. Veuillez entrer une adresse IP valide."
+        echo -e "$red Adresse IP invalide. Veuillez entrer une adresse IP valide. $clear"
         continue
     else
-        echo "Adresse IP valide : $adresse_ip"
+        echo -e "$green Adresse IP valide : $adresse_ip $clear"
     fi
     if ! [[ $masque_sous_reseau =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo "Masque de sous-réseau invalide. Veuillez entrer un masque valide (ex: 255.255.255.0)."
+        echo -e "$red Masque de sous-réseau invalide. Veuillez entrer un masque valide (ex: 255.255.255.0). $clear "
         continue
     else
-        echo "Masque de sous-réseau valide : $masque_sous_reseau"
+        echo -e "$green Masque de sous-réseau valide : $masque_sous_reseau $clear"
     fi
     # Configuration du réseau avec les informations fournies, Changer "eth0" selon votre interface réseau
-    sudo ip addr add $adresse_ip/$masque_sous_reseau dev eth0 || { echo "Erreur lors de la configuration de l'adresse IP."; break; }
-    sudo ip route add default via $passerelle || { echo "Erreur lors de l'ajout de la passerelle."; break; }
-    echo "nameserver $dns_primaire" | sudo tee /etc/resolv.conf > /dev/null || { echo "Erreur lors de la configuration du DNS primaire."; break; }
-    echo "nameserver $dns_secondaire" | sudo tee -a /etc/resolv.conf > /dev/null || { echo "Erreur lors de la configuration du DNS secondaire."; break; }
+    sudo ip addr add $adresse_ip/$masque_sous_reseau dev eth0 || { echo -e "$red Erreur lors de la configuration de l'adresse IP. $clear"; break; }
+    sudo ip route add default via $passerelle || { echo -e "$red Erreur lors de l'ajout de la passerelle. $clear"; break; }
+    echo "nameserver $dns_primaire" | sudo tee /etc/resolv.conf > /dev/null || { echo -e "$red Erreur lors de la configuration du DNS primaire. $clear"; break; }
+    echo "nameserver $dns_secondaire" | sudo tee -a /etc/resolv.conf > /dev/null || { echo -e "$red Erreur lors de la configuration du DNS secondaire. $clear"; break; }
     # Affichage des informations renseignées
-    echo "Informations renseignées :"
+    echo -e "$green_background Informations renseignées : $clear "
     echo "Adresse IP : $adresse_ip"
     echo "Masque de sous-réseau : $masque_sous_reseau"
     echo "Passerelle par défaut : $passerelle"
@@ -151,7 +152,8 @@ done
 }
 
 while true; do
-    echo "===== Configuration Menu ====="
+    clear
+    echo -e "$green_background===== Configuration Menu =====$clear"
     echo "1.Configuration du Nom d'Hôte "
     echo "2.Création d'un Nouvel Utilisateur "
     echo "3.Installation de logiciels "
